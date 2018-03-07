72b5de8671063c5776402506686cec83fa2b0e0c
Dele Olajide <dele@4ng.net>
2018-03-07 00:41:10 +0000

    cleanup

--- src/java/org/ifsoft/cakeshop/openfire/PluginImpl.java
+++ src/java/org/ifsoft/cakeshop/openfire/PluginImpl.java
@@ -1,12 +1,6 @@
 package org.ifsoft.cakeshop.openfire;
 
 import java.io.File;
-import java.net.*;
-import java.util.concurrent.*;
-import java.util.*;
-import java.util.function.*;
-import java.util.stream.*;
-import java.nio.file.*;
 
 import org.jivesoftware.openfire.container.Plugin;
 import org.jivesoftware.openfire.container.PluginManager;
@@ -35,19 +29,14 @@ import org.eclipse.jetty.security.authentication.*;
 import org.apache.tomcat.InstanceManager;
 import org.apache.tomcat.SimpleInstanceManager;
 
-import java.lang.reflect.*;
 import java.util.*;
 
-import org.jitsi.util.OSUtils;
-import com.jpmorgan.cakeshop.config.*;
 
 
-public class PluginImpl implements Plugin, PropertyEventListener, ProcessListener
+public class PluginImpl implements Plugin, PropertyEventListener
 {
     private static final Logger Log = LoggerFactory.getLogger(PluginImpl.class);
-
-    private ServletContextHandler cakeshopContext;
-    private ServletContextHandler apiContext;
+    private WebAppContext context4;
 
 
     public void destroyPlugin()
@@ -55,103 +44,39 @@ public class PluginImpl implements Plugin, PropertyEventListener, ProcessListene
         PropertyEventDispatcher.removeListener(this);
 
         try {
-            HttpBindManager.getInstance().removeJettyHandler(cakeshopContext);
-            HttpBindManager.getInstance().removeJettyHandler(apiContext);
+            HttpBindManager.getInstance().removeJettyHandler(context4);
         }
         catch (Exception e) {
-            //Log.error("IPFS destroyPlugin ", e);
+
         }
     }
 
     public void initializePlugin(final PluginManager manager, final File pluginDirectory)
     {
         PropertyEventDispatcher.addListener(this);
-        checkNatives(pluginDirectory);
 
         boolean cakeshopEnabled = JiveGlobals.getBooleanProperty("cakeshop.enabled", true);
 
         if (cakeshopEnabled)
         {
-            addCakeshopProxy();
+            Log.info("Initialize Cakeshop");
+
+            context4 = new WebAppContext(null, pluginDirectory.getPath() + "/classes/war", "/cakeshop");
+
+            final List<ContainerInitializer> initializers4 = new ArrayList();
+            initializers4.add(new ContainerInitializer(new JettyJasperInitializer(), null));
+            context4.setAttribute("org.eclipse.jetty.containerInitializers", initializers4);
+            context4.setAttribute(InstanceManager.class.getName(), new SimpleInstanceManager());
+
+/*
+            context4 = new WebAppContext();
+            context4.setWar( pluginDirectory.getPath() + "/classes/cakeshop.war" );
+            context4.setContextPath( "/cakeshop" );
+*/
+            HttpBindManager.getInstance().addJettyHandler(context4);
 
         } else {
             Log.info("cakeshop disabled");
-        }
-    }
-
-    public String getIpAddress()
-    {
-        String ourHostname = XMPPServer.getInstance().getServerInfo().getHostname();
-        String ourIpAddress = "127.0.0.1";
-
-        try {
-            ourIpAddress = InetAddress.getByName(ourHostname).getHostAddress();
-        } catch (Exception e) {
-
-        }
-
-        return ourIpAddress;
-    }
-
-
-
-    private void addCakeshopProxy()
-    {
-        Log.info("Initialize CakeshopProxy");
-
-        cakeshopContext = new ServletContextHandler(null, "/cakeshop", ServletContextHandler.SESSIONS);
-        cakeshopContext.setClassLoader(this.getClass().getClassLoader());
-
-        ServletHolder proxyServlet = new ServletHolder(ProxyServlet.Transparent.class);
-        proxyServlet.setInitParameter("proxyTo", "http://127.0.0.1:8080/cakeshop");
-        proxyServlet.setInitParameter("prefix", "/");
-        cakeshopContext.addServlet(proxyServlet, "/*");
-
-        HttpBindManager.getInstance().addJettyHandler(cakeshopContext);
-
-        apiContext = new ServletContextHandler(null, "/api", ServletContextHandler.SESSIONS);
-        apiContext.setClassLoader(this.getClass().getClassLoader());
-
-        ServletHolder proxyServlet2 = new ServletHolder(ProxyServlet.Transparent.class);
-        proxyServlet2.setInitParameter("proxyTo", "http://127.0.0.1:8102");
-        proxyServlet2.setInitParameter("prefix", "/");
-        apiContext.addServlet(proxyServlet2, "/*");
-
-        HttpBindManager.getInstance().addJettyHandler(apiContext);
-    }
-
-    private void checkNatives(File pluginDirectory)
-    {
-        try
-        {
-            String suffix = null;
-
-            if(OSUtils.IS_LINUX32)
-            {
-                suffix = "linux-32";
-            }
-            else if(OSUtils.IS_LINUX64)
-            {
-                suffix = "linux-64";
-            }
-            else if(OSUtils.IS_WINDOWS64)
-            {
-                suffix = "win-64";
-            }
-
-            if (suffix != null)
-            {
-
-                Log.info("checkNatives cakeshop OS " + suffix);
-
-            } else {
-
-                Log.error("checkNatives unknown OS " + pluginDirectory.getAbsolutePath());
-            }
-        }
-        catch (Exception e)
-        {
-            Log.error(e.getMessage(), e);
         }
     }
 
